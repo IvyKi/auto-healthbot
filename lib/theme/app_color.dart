4a506735d9c7ddeb1203b4ec90b01c5cc50aa3f4
IvyKi <kinahae0331@tukorea.ac.kr>
2025-04-14 15:41:33 +0900

    Feat: app color palette

---
+++ lib/theme/app_color.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class Gradients1{
+  static const color1 = Color(0xff7EB6FF);
+  static const color2 = Color(0xff80A9BE);
+  static const color3 = Color(0xff829C7D);
+}
+
+class Gradients2{
+  static const blue = Color(0xff7EB6FF);
+  static const purple = Color(0xffAE9FC8);
+  static const pink = Color(0xffDF8892);
+  static const red = Color(0xffFF796D);
+}
+
+class AppColor{
+  static const blue = Color(0xff7EB6FF);
+  static const green = Color(0xff829C7D);
+  static const red = Color(0xffFF796D);
+}
