40d033c130d6538f70abd238b70d21859cabd15a
IvyKi <kinahae0331@tukorea.ac.kr>
2025-06-11 16:28:38 +0900

    Chore: 화면 비율에 따라 좌표영역 취득되게 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:auto_healthbot/services/CoordinatePicker.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/splash_screen.dart';
